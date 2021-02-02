e69b65d97a098390d11e2593d4fc370546cd32d6
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2021-02-02 22:47:16 +0100

    fixes client with the new API.

--- src/main/java/fr/univtln/bruno/samples/jaxrs/client/BiblioClient.java
+++ src/main/java/fr/univtln/bruno/samples/jaxrs/client/BiblioClient.java
@@ -7,29 +7,33 @@ import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.MediaType;
+import lombok.extern.java.Log;
 
 /**
  * Created by bruno on 04/11/14.
  */
 
+@Log
 public class BiblioClient {
     public static void main(String[] args) {
         // create the client
         Client client = ClientBuilder.newClient();
-        WebTarget webResource = client.target(BiblioServer.BASE_URI);
+        WebTarget webResource = client.target("http://localhost:9998/myapp");
 
-        //Send a get with a String as response
-        String responseAuteursAsJson = webResource.path("biblio/auteurs").request().get(String.class);
-        System.out.println(responseAuteursAsJson);
+        //Send a put with a String as response
+        String responseInitAsString = webResource.path("biblio/init")
+                .request().put(Entity.entity("", MediaType.TEXT_PLAIN), String.class);
+        log.info(responseInitAsString);
+
+        //Send a get and parse the response as a String
+        String responseAuteursAsJsonString = webResource.path("biblio/auteurs")
+                .request().get(String.class);
+        log.info(responseAuteursAsJsonString);
 
         //Idem but the result is deserialised to an instance of Auteur
-        Auteur auteur = webResource.path("biblio/auteur/1").request().get(Auteur.class);
-        System.out.println(auteur);
-
-        webResource.path("biblio/auteur")
-                .queryParam("nom", "John").queryParam("prenom", "Smith")
-                .request().put(Entity.entity(Auteur.builder().prenom("John").nom("Smith").build(),
-                MediaType.APPLICATION_JSON));
-        System.out.println(webResource.path("biblio/auteurs").request().get(String.class));
+        Auteur auteur = webResource.path("biblio/auteurs/1")
+                .request()
+                .get(Auteur.class);
+        log.info(auteur.toString());
     }
 }
