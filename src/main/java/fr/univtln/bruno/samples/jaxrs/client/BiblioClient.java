522e22d610241db416306ccbf18094caeb5fb222
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2021-04-07 12:18:27 +0200

    fixes client to english API.3

--- src/main/java/fr/univtln/bruno/samples/jaxrs/client/BiblioClient.java
+++ src/main/java/fr/univtln/bruno/samples/jaxrs/client/BiblioClient.java
@@ -17,20 +17,20 @@ public class BiblioClient {
     public static void main(String[] args) {
         // create the rest client
         Client client = ClientBuilder.newClient();
-        WebTarget webResource = client.target("http://localhost:9998/myapp");
+        WebTarget webResource = client.target("http://localhost:9998/mylibrary");
 
         //Send a put with a String as response
-        String responseInitAsString = webResource.path("biblio/init")
+        String responseInitAsString = webResource.path("library/init")
                 .request().put(Entity.entity("", MediaType.TEXT_PLAIN), String.class);
         log.info(responseInitAsString);
 
         //Send a get and parse the response as a String
-        String responseAuteursAsJsonString = webResource.path("biblio/authors")
+        String responseAuteursAsJsonString = webResource.path("authors")
                 .request().get(String.class);
         log.info(responseAuteursAsJsonString);
 
         //Idem but the result is deserialised to an instance of Auteur
-        Author author = webResource.path("biblio/authors/1")
+        Author author = webResource.path("authors/1")
                 .request()
                 .get(Author.class);
         log.info(author.toString());
@@ -38,7 +38,7 @@ public class BiblioClient {
         //Log in to get the token with basci authentication
         String email = "john.doe@nowhere.com";
         String passwd = "admin";
-        String token = webResource.path("biblio/login")
+        String token = webResource.path("setup/login")
                 .request()
                 .accept(MediaType.TEXT_PLAIN)
                 .header("Authorization", "Basic " + java.util.Base64.getEncoder().encodeToString((email + ":" + passwd).getBytes()))
@@ -46,7 +46,7 @@ public class BiblioClient {
         if (!token.isBlank()) {
             log.info("token received.");
             //We access a JWT protected URL with the token
-            String result = webResource.path("biblio/secured")
+            String result = webResource.path("setup/secured")
                     .request()
                     .header("Authorization", "Bearer " + token)
                     .get(String.class);
